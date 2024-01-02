3f7ca1e7ce8d8176b421f2d2354990f794654682
Jean-Gaëtan De Souza <130936521+LeCodeByJean@users.noreply.github.com>
2024-01-02 18:23:35 +0100

    Update poly.py

--- Projects/Modules/OOP/Unit5/poly.py
+++ Projects/Modules/OOP/Unit5/poly.py
@@ -1,3 +1,11 @@
+# Program name: Poly.py
+# By Jean-G. De Souza
+# Essex University Online
+# PG Cert in Computer Science / Module OOP / Unit 5
+# Activity instructions: Write a Python program with polymorphism that is usable within the summative assessment for the driverless car.
+
+# --------------------------
+
 class DriverlessCar:
     '''Class for a driverless Car. '''
 
