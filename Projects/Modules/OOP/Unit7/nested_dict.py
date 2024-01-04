49801ecbd36130bf9e1887cd98cdc2e65185a66c
Jean-Gaëtan De Souza <130936521+LeCodeByJean@users.noreply.github.com>
2024-01-04 09:49:48 +0000

    edit nested_dict.py formatting

--- Projects/Modules/OOP/Unit7/nested_dict.py
+++ Projects/Modules/OOP/Unit7/nested_dict.py
@@ -1,10 +1,10 @@
 # By Jean-G. De Souza
 # Essex University Online
 # PG Cert in Computer Science - Module OOP: Unit 7
-# Activity instructions: Create a nested dictionary of data on cars within a Car class. Extend the program to work with the dictionary by calling the following methods:
-# items()
-# keys()
-# values()
+# Activity instructions:
+#   " Create a nested dictionary of data on cars within a Car class.
+#     Extend the program to work with the dictionary by calling the following methods:
+#     items(), keys(), values() "
 
 # --------------------------
 
