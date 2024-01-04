fc26c1a10c0d29207c39a6c86d672f2a64c4d40f
Jean-Gaëtan De Souza <130936521+LeCodeByJean@users.noreply.github.com>
2024-01-04 17:53:28 +0000

    module docstring

--- Projects/Modules/OOP/Unit7/nested_dict.py
+++ Projects/Modules/OOP/Unit7/nested_dict.py
@@ -1,12 +1,15 @@
-# By Jean-G. De Souza
-# Essex University Online
-# PG Cert in Computer Science - Module OOP: Unit 7
-# Activity instructions:
-#   " Create a nested dictionary of data on cars within a Car class.
-#     Extend the program to work with the dictionary by calling the following methods:
-#     items(), keys(), values() "
+'''
+By Jean-G. De Souza
+Essex University Online
+PG Cert in Computer Science - Module OOP: Unit 7
+Activity instructions:
+  " Create a nested dictionary of data on cars within a Car class.
+    Extend the program to work with the dictionary by calling the following methods:
+    items(), keys(), values() "
+'''
 
 # --------------------------
 
 class Car:
     '''class for cars'''
+    
