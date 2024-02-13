7d8a05481a88d3da544e0e19f2e930f977af1052
Jean-Gaëtan De Souza <130936521+LeCodeByJean@users.noreply.github.com>
2024-02-13 17:26:40 +0100

    Finnished Activity4.py

--- Projects/Modules/OOP/Unit9/Activity4.py
+++ Projects/Modules/OOP/Unit9/Activity4.py
@@ -1,7 +1,7 @@
 '''
 By Jean-G. De Souza
 Essex University Online
-PG Cert in Computer Science - Module OOP: Unit 9
+PG Cert in Computer Science - Module OOP: Unit 9 - Activity 4
 
     Activity instructions:
   " Extend the following program to test accuracy of operations using the assert statement:
@@ -15,3 +15,22 @@ PG Cert in Computer Science - Module OOP: Unit 9
 '''
 
 # --------------------------
+
+
+# Python String Operations
+str1 = 'Hello'
+str2 = 'World!'
+
+# using +
+concatenated = str1 + str2
+print('str1 + str2 = ', concatenated)
+# Test for concat.
+assert concatenated == 'HelloWorld!', f"Unexpected result. Expecting 'HelloWorld!' instead of '{concatenated}'."
+
+# using *
+multiplied = str1 * 3
+print('str1 * 3 =', multiplied)
+# Test for multiply
+assert multiplied == 'HelloHelloHello', f"Unexpected result. Expecting 'HelloHelloHello' instead of '{multiplied}'. "
+
+print("All tests passed!")
