bb96d50a47de49c54c11b5ead831b6bd9bd1100c
Jean-Gaëtan De Souza <130936521+LeCodeByJean@users.noreply.github.com>
2024-02-13 17:04:06 +0100

    Create Unit9 folder and base of Activity4.py

---
+++ Projects/Modules/OOP/Unit9/Activity4.py
@@ -0,0 +1,17 @@
+'''
+By Jean-G. De Souza
+Essex University Online
+PG Cert in Computer Science - Module OOP: Unit 9
+
+    Activity instructions:
+  " Extend the following program to test accuracy of operations using the assert statement:
+            # Python String Operations
+            str1 = 'Hello'
+            str2 ='World!'
+            # using +
+            print('str1 + str2 = ', str1 + str2)
+            # using *
+            print('str1 * 3 =', str1 * 3)         "
+'''
+
+# --------------------------
